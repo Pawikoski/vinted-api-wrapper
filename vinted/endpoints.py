b5f0b53f50176138fe4f08e111a880ffbdaa41e8
Paweł Stawikowski <pawikoski@gmail.com>
2024-09-27 14:25:13 +0200

    Refactor Endpoints to Enum collection

--- vinted/endpoints.py
+++ vinted/endpoints.py
@@ -1,8 +1,14 @@
-CATALOG_ITEMS = "/catalog/items"
-CATALOG_FILTERS = "/catalog/filters"
-CATALOG_INITIALIZERS = "/catalog/initializers"
-ITEMS = "/items"
-USERS = "/users"
-USER_FEEDBACKS = "/user_feedbacks"
-USER_FEEDBACKS_SUMMARY = "/user_feedbacks/summary"
-SEARCH_SUGGESTIONS = "/search_suggestions"
+from enum import Enum
+
+
+class Endpoints(Enum):
+    CATALOG_ITEMS = "/catalog/items"
+    CATALOG_FILTERS = "/catalog/filters"
+    CATALOG_INITIALIZERS = "/catalog/initializers"
+    ITEMS = "/items/{}"
+    USERS = "/users"
+    USER = "/users/{}"
+    USER_FEEDBACKS = "/user_feedbacks"
+    USER_ITEMS = "/users/{}/items"
+    USER_FEEDBACKS_SUMMARY = "/user_feedbacks/summary"
+    SEARCH_SUGGESTIONS = "/search_suggestions"
