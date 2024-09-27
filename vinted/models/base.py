abb3561010b2c9f11e5f6e5bec6806ea1e752650
Paweł Stawikowski <pawikoski@gmail.com>
2024-09-27 14:37:50 +0200

    Create response_codes file with all possible response codes from vinted and make VintedResponse.code Literal of these codes

--- vinted/models/base.py
+++ vinted/models/base.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Literal
 
 
 @dataclass
@@ -13,5 +13,52 @@ class Pagination:
 
 @dataclass
 class VintedResponse:
-    code: int
+    code: Literal[
+        -200,
+        -100,
+        -99,
+        0,
+        10,
+        20,
+        21,
+        99,
+        50,
+        51,
+        52,
+        100,
+        102,
+        103,
+        104,
+        105,
+        106,
+        107,
+        108,
+        110,
+        112,
+        113,
+        114,
+        115,
+        116,
+        117,
+        118,
+        119,
+        121,
+        122,
+        123,
+        124,
+        127,
+        128,
+        129,
+        130,
+        131,
+        134,
+        136,
+        140,
+        142,
+        143,
+        146,
+        152,
+        156,
+        158,
+    ]
     pagination: Optional[Pagination]
