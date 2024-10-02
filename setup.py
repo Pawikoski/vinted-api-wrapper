fb0e26c359c10ecb7616221a29f37182de8f2b79
Paweł Stawikowski <pawikoski@gmail.com>
2024-10-02 12:33:40 +0200

    Create setup to build and publish pypi package

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="vinted-api-wrapper",
+    version="0.1.1",
+    description="Unofficial Wrapper for Vinted API",
+    author="Paweł Stawikowski",
+    author_email="pawikoski@gmail.com",
+    packages=find_packages(),
+    url="https://github.com/Pawikoski/vinted-api-wrapper",
+    install_requires=["requests", "dacite"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
