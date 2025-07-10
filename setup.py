d623e6b4e1f28e3bba88153d7931492654daa603
Serv010 <86478284+Serv010@users.noreply.github.com>
2025-07-10 11:27:58 +0200

    Update setup.py
    added encoding="utf-8" so we can use pip install properly

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-with open("README.md", "r") as readme_file:
+with open("README.md", encoding="utf-8") as readme_file:
     long_description = readme_file.read()
 
 setup(
